04914a4cb27b56e51c93def22b02a488e2df2da4
Puffin Kwadraat <80580843+PuffinKwadraat@users.noreply.github.com>
2021-03-21 09:45:11 +0100

    Add files via upload

---
+++ Discord Bot/Commands/apply.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'apply',
+    description: "This is an apply command.",
+    execute(message, args){
+        message.channel.send('If you would like to apply to one or our roles, please fill in this form: https://forms.gle/pabpqPiWeBp9aZ2a8');
+    }
+}
