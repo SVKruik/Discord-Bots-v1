6174be87cf715e565ee376eb2dd7af161cf6597c
Puffin Kwadraat <80580843+PuffinKwadraat@users.noreply.github.com>
2021-03-21 09:47:15 +0100

    Delete apply.js

--- apply.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'apply',
-    description: "This is an apply command.",
-    execute(message, args){
-        message.channel.send('If you would like to apply to one or our roles, please fill in this form: https://forms.gle/pabpqPiWeBp9aZ2a8');
-    }
-}
