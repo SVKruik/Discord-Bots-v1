1b7703c6c7750d632ae3bef2b714b4d16078eff2
PuffinKwadraat <sv.kruik@gmail.com>
2021-06-08 21:35:53 +0200

    Advanced Event

--- Epic Gamer Server Bot/Commands/newevent.js
+++ Epic Gamer Server Bot/Commands/newevent.js
@@ -3,24 +3,32 @@ module.exports = {
     permissions: ["ADMINISTRATOR"],
     description: "Sets up a reaction role message!",
     async execute(message, args, cmd, client, Discord) {
-        const channel = '828557449975693342';
-        const redTeamRole = message.guild.roles.cache.find(role => role.name === "Team Red");
-        const blueTeamRole = message.guild.roles.cache.find(role => role.name === "Team Blue");
+        const channel = '820580470207021058';
+        const greenTeamRole = message.guild.roles.cache.find(role => role.name === "TeamGreen");
+        const redTeamRole = message.guild.roles.cache.find(role => role.name === "TeamRed");
+        const yellowTeamRole = message.guild.roles.cache.find(role => role.name === "TeamYellow");
+        const whiteTeamRole = message.guild.roles.cache.find(role => role.name === "TeamWhite");
  
+        const greenTeamEmoji = '🟢';
         const redTeamEmoji = '🔴';
-        const blueTeamEmoji = '🔵';
+        const yellowTeamEmoji = '🟡';
+        const whiteTeamEmoji = '⚪';
  
         let embed = new Discord.MessageEmbed()
             .setColor('#ffb1b1')
-            .setFooter('Stats and bot profided by Complex. Usage: -reactionrole.')
+            .setFooter('Stats and bot profided by Complex. Usage: -newevent.')
             .setTitle('Choose a team to play on!')
             .setDescription('Choosing a team will allow you to interact with your teammates while in a event! When a event starts with for example Bed Wars, you can choose your team here. Be aware though that teams will be balanced, and your pick is not always available.\n\n'
-                + `${redTeamEmoji} for Red team\n`
-                + `${blueTeamEmoji} for Blue team`);
+                + `${greenTeamEmoji} for the Green team\n`
+                + `${redTeamEmoji} for the Red team\n`
+                + `${yellowTeamEmoji} for the Yellow team\n`
+                + `${whiteTeamEmoji} for the White team`);
  
         let messageEmbed = await message.channel.send(embed);
+        messageEmbed.react(greenTeamEmoji);
         messageEmbed.react(redTeamEmoji);
-        messageEmbed.react(blueTeamEmoji);
+        messageEmbed.react(yellowTeamEmoji);
+        messageEmbed.react(whiteTeamEmoji);
  
         client.on('messageReactionAdd', async (reaction, user) => {
             if (reaction.message.partial) await reaction.message.fetch();
@@ -29,11 +37,14 @@ module.exports = {
             if (!reaction.message.guild) return;
  
             if (reaction.message.channel.id == channel) {
-                if (reaction.emoji.name === redTeamEmoji) {
+                if (reaction.emoji.name === greenTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(greenTeamRole);
+                } if (reaction.emoji.name === redTeamEmoji) {
                     await reaction.message.guild.members.cache.get(user.id).roles.add(redTeamRole);
-                }
-                if (reaction.emoji.name === blueTeamEmoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.add(blueTeamRole);
+                } if (reaction.emoji.name === yellowTeamEmoji) {
+                        await reaction.message.guild.members.cache.get(user.id).roles.add(yellowTeamRole);
+                } if (reaction.emoji.name === whiteTeamEmoji) {
+                        await reaction.message.guild.members.cache.get(user.id).roles.add(whiteTeamRole);
                 }
             } else {
                 return;
@@ -50,11 +61,14 @@ module.exports = {
  
  
             if (reaction.message.channel.id == channel) {
-                if (reaction.emoji.name === redTeamEmoji) {
+                if (reaction.emoji.name === greenTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(greenTeamRole);
+                } if (reaction.emoji.name === redTeamEmoji) {
                     await reaction.message.guild.members.cache.get(user.id).roles.remove(redTeamRole);
-                }
-                if (reaction.emoji.name === blueTeamEmoji) {
-                    await reaction.message.guild.members.cache.get(user.id).roles.remove(blueTeamRole);
+                } if  (reaction.emoji.name === yellowTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(yellowTeamRole);
+                } if  (reaction.emoji.name === whiteTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(whiteTeamRole);
                 }
             } else {
                 return;
