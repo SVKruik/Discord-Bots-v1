eebce7d2616bef41ff0a47bd2e5aca21af01576b
PuffinKwadraat <sv.kruik@gmail.com>
2021-06-08 21:05:16 +0200

    Withdraw, Deposit and Give

--- Epic Gamer Server Bot/Commands/balance.js
+++ Epic Gamer Server Bot/Commands/balance.js
@@ -31,10 +31,10 @@ module.exports = {
 
       if (reaction.message.channel.id == channel) {
         if (reaction.emoji.name === BankEmoji) {
-          await message.channel.send(`This is your current bank balance : \`${profileData.bank}\`.`);
+          await message.channel.send(`${message.author.username}, this is your current bank balance : \`${profileData.bank}\`.`);
         }
         if (reaction.emoji.name === WalletEmoji) {
-          await message.channel.send(`This is your current wallet balance : \`${profileData.coins}\`.`);
+          await message.channel.send(`${message.author.username}, this is your current wallet balance : \`${profileData.coins}\`.`);
         }
       } else {
         return;
