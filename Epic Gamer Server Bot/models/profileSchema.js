bb1f5b946ac41eef6d22497f94ded4bc32cc990d
PuffinKwadraat <sv.kruik@gmail.com>
2021-09-05 20:22:45 +0200

    Update

--- Epic Gamer Server Bot/models/profileSchema.js
+++ Epic Gamer Server Bot/models/profileSchema.js
@@ -4,11 +4,11 @@ const profileSchema = new mongoose.Schema({
   userID: { type: String, require: true, unique: true },
   serverID: { type: String, require: true },
   coins: { type: Number, default: 0 },
-  coins: { type: Number },
+  coins: { type: Number, default: 0},
   level: { type: Number, default: 0},
   name: { type: String, require: true, unique : false},
 });
 
-const model = mongoose.model("ProfileModels", profileSchema);
+const model = mongoose.model("users", profileSchema);
 
 module.exports = model;
