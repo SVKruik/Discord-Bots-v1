4c8aaf234ba8c139b4654cd3a61ae5bd99767429
PuffinKwadraat <sv.kruik@gmail.com>
2021-10-01 17:04:46 +0200

    Deleted TMCB
    New bot pending.

--- Discord Bots/XX. MultiCube/Commands/balancebank.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'balbank',
-    permissions: ["SEND_MESSAGES"],
-    description: "This command shows your bank balance.",
-    async execute(message, args, cmd, client, Discord, profileData) {
-        message.channel.send(`${message.author.username}, this is your current bank balance : \`${profileData.bank}\`.`);
-    }
-}
--- Discord Bots/XX. MultiCube/Commands/cmdtemplate.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'cmdtemplate',
-    description: 'Command template for devs.',
-    permissions: ["ADMINISTRATOR"],
-    execute(message, args, cmd, client, Discord) {
-        message.channel.send('Template')
-    }
-}
