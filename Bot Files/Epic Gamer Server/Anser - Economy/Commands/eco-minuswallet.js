45e81096ad78d177e32b2a7e1869f783cd0bbf92
Puffin Kwadraat <sv.kruik@gmail.com>
2021-11-25 12:46:12 +0100

    Anser

---
+++ Bot Files/Epic Gamer Server/Anser - Economy/Commands/eco-minuswallet.js
@@ -0,0 +1,38 @@
+const profileModel = require("../models/profileSchema");
+module.exports = {
+  name: "minuswallet",
+  aliases: [],
+  cooldown: process.env.ASADMIN,
+  permissions: ["ADMINISTRATOR"],
+  description: "Current - amount (wallet).",
+  async execute(message, args, cmd, client, discord, profileData) {
+    if (!args.length) return message.channel.send(process.env.MSGMEMBERMENTION);
+    const amount = args[1];
+    const target = message.mentions.users.first();
+    if (!target) return message.channel.send(process.env.MSGFINDERR);
+
+    if (amount % 1 != 0 || amount < 0)
+      return message.channel.send(process.env.MSGGREATERONE);
+
+    try {
+      const targetData = await profileModel.findOne({ userID: target.id });
+      if (!targetData) return message.channel.send(process.env.MSGNOACCOUNT);
+      await profileModel.findOneAndUpdate(
+        {
+          userID: target.id,
+        },
+        {
+          $inc: {
+            coins: -amount,
+          },
+        }
+      );
+
+      return message.channel.send(
+        `${message.author.username}, the targeted member has lost \`${amount}\` amount of coins (wallet).`
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
