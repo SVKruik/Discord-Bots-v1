0be73060bb0195821eb0d4ab3cb7d6ce7bccfd3d
PuffinKwadraat <sv.kruik@gmail.com>
2022-01-12 18:41:28 +0100

    Upgraded the 'Ban' command. Now also logs the information and upgraded the 'reason' parameter.

--- Bot Files/Epic Gamer Server/Ciconia - All/Commands/admin-ban.js
+++ Bot Files/Epic Gamer Server/Ciconia - All/Commands/admin-ban.js
@@ -1,3 +1,5 @@
+require('console-stamp')(console, '[HH:MM]');
+
 module.exports = {
   name: "ban",
   aliases: [],
@@ -8,15 +10,18 @@ module.exports = {
     const target = message.mentions.users.first();
     const username = message.client.user.username;
 
+    const reason1 = args[1]
+    const reason2 = reason1.charAt(0).toUpperCase() + reason1.slice(1);
+    
     if (target) {
       const memberTarget = message.guild.members.cache.get(target.id);
       memberTarget.ban();
+      console.log(`${memberTarget.user.username} got banned because of: ${reason2}. This was done by ${message.author.username} using ${username}.`);
       message.channel.send(
-        `<@${memberTarget.user.id}> got 360 no-scoped by ${username}. He/she is no longer with us.`
+        `<@${memberTarget.user.id}> got 360 no-scoped by ${username}. Reason: ${reason2}.`
       );
     } else {
       message.channel.send(process.env.MSGERR);
     }
   },
 };
-//`<@${memberTarget.user.id}> got 360 no-scoped by the MultiCube Community Bot! Reason \`${d.d}\`.`
