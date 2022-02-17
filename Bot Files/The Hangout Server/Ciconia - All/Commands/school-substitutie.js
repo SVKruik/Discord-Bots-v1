92e74b69b1980c8611229a5c3c0d46489236c857
PuffinKwadraat <sv.kruik@gmail.com>
2022-02-17 19:31:18 +0100

    Substitute fixing 50%

--- Bot Files/The Hangout Server/Ciconia - All/Commands/school-substitutie.js
+++ Bot Files/The Hangout Server/Ciconia - All/Commands/school-substitutie.js
@@ -9,12 +9,15 @@ module.exports = {
   execute(message, args, cmd, client, Discord) {
     const username = message.client.user.id;
 
-    const cirkelnaam = "C";
-    const formulenaam = "Q";
+    const cirkela = args[0]; // A
+    const cirkelb = args[1]; // B
+    const cirkelc = args[2]; // C
 
-    const afcirkela = args[1];
-    const afcirkelb = args[2];
-    const afcirkelc = args[3];
+    const formulea = args[3]; // A
+    const formulexlow = `${args[5]}`; // B
+    const formulex = formulexlow.toUpperCase();
+
+    const cirkelnaam = args[4]; // c: ...
 
     if (!args[0]) {
       return message.channel.send("You are missing the value \`A\` of the circle formula.")
@@ -25,67 +28,35 @@ module.exports = {
     } else if (!args[3]) {
       return message.channel.send("You are missing the value \`A\` of the line formula.")
     } else if (!args[4]) {
-      return message.channel.send("You are missing the value \`B\` of the line formula.")
+      return message.channel.send("You are missing the name of your circle formula.")
+    } else if (!args[5]) {
+      return message.channel.send("You are missing the name of your variable in the line formula.")
     }
 
-    const afcirkelakwa = afcirkela * afcirkela;
-    const afcirkelbkwa = afcirkelb * afcirkelb;
-    const cirkela2 = afcirkela * 2;
-    const cirkelb2 = afcirkelb * 2;
+    const cirkela2 = cirkela * 2
+    const cirkelb2 = cirkelb * 2
 
-    const cirkeloverigen = afcirkelakwa + afcirkelbkwa - afcirkelc;
-    const cirkelbegin = "X² + Y²";
-    //AX - BY = C
-    const aformule = args[4];
-    const bformule = args[5];
-    const cformule = formulenaam;
-    //AX + B
-    const aformuleom = aformule * -1;
-    const bformuleom = bformule * -1;
-    const cformuleom = cformule;
+    const cirkelakwa = cirkela * cirkela
+    const cirkelbkwa = cirkelb * cirkelb
+    const cirkeloverigen = cirkelakwa + cirkelbkwa - cirkelc
 
-    const aformuleomkwa = aformuleom * aformuleom;
-    const aformuleom2 = aformuleom * 2;
-    const cformuleomkwa = cformuleom * cformuleom;
-    const aformuleomkwatot = aformuleomkwa + 1;
+    const formulea2 = formulea * 2
+    const formuleakwa = formulea * formulea 
 
-    const cirkelb2keeraformuleom = cirkelb2 * aformuleom;
-    const cirkelb2wis = cirkelb2 * -1;
-    const cirkela2cirkelb2keeraformuleom = cirkela2 + cirkelb2keeraformuleom;
-    const bthreeabcformule =
-      cirkela2cirkelb2keeraformuleom * cirkela2cirkelb2keeraformuleom;
-    const aformuleom2kwa = aformuleom2 * aformuleom2;
-    const btwoabcformuleonvol = aformuleom2 * cirkela2cirkelb2keeraformuleom;
-    const btwoabcformule = btwoabcformuleonvol * -2;
+    const xkwavol = formuleakwa + 1
 
-    const aabcformuleonvol = 4 * aformuleomkwatot;
-    const aabcformule = aabcformuleonvol * -1;
+    const formulea2vol = formulea2 - cirkela2
 
-    const coneabcformuleonvol = cirkelb2wis * aabcformule * -1;
-    const coneabcformule = coneabcformuleonvol * -1;
+    const formulea2kwa = formulea2 * formulea2
+    const formulea2kwacon = formulea2 * 2
+    const formulea2kwac = formulea2kwacon * formulea2vol
+    const formulea2volkwa = formulea2vol * formulea2vol
 
-    const ctwoabcformuleonvol = aabcformule * cirkeloverigen * -1;
-    const ctwoabcformule = ctwoabcformuleonvol * -1;
-
-    const aabcformuleaf = aformuleom2kwa + aabcformule;
-    const babcformuleaf = btwoabcformule + coneabcformule;
-    const cabcformuleaf = bthreeabcformule + ctwoabcformule;
-
-    const aabcformuleafdeelaabc = aabcformuleaf / aabcformuleaf;
-    const babcformuleafdeelaabc = babcformuleaf / aabcformuleaf;
-    const cabcformuleafdeelaabc = cabcformuleaf / aabcformuleaf;
-
-    if (args[5] === "delete") {
+    if (args[6] === "delete") {
       message.delete();
-    } else if (args[5] === "del") {
-      message.delete();
-    } else if (args[5] === "d") {
+    } else if (args[6] === "del" || args[6] === "d") {
       message.delete();
     }
-
-    //Input
-    // var coordpuntenvolafgerond =
-    //   Math.round((coordpuntenvol + Number.EPSILON) * 10) / 10;
 
     const newEmbed = new Discord.MessageEmbed()
       .setColor(config.base.basecolor)
@@ -95,55 +66,63 @@ module.exports = {
       .addFields(
         {
           name: `1. Opdracht:`,
-          value: `Gegeven is de cirkel ${cirkelnaam}: (X ${afcirkela})² + (Y ${afcirkelb})² = ${afcirkelc}. Bereken voor welke waarden van ${formulenaam} de lijn ${aformule} ${bformule} = ${cformule} de cirkel raakt.`,
+          value: `Gegeven is de cirkel ${cirkelnaam}: (X - ${cirkela})² + (Y- ${cirkelb})² = ${cirkelc}. Bereken voor welke waarden van ${formulex} de lijn Y = ${formulea}X + ${formulex} de cirkel raakt.`,
         },
         {
           name: `2. Cirkel formule opstellen:`,
-          value: `\`\`\`(X ${afcirkela})² + (Y ${afcirkelb})² = ${afcirkelc}. Geeft X² ${cirkela2}X + ${afcirkelakwa} + Y² ${cirkelb2}Y + ${afcirkelbkwa} = ${afcirkelc}. Rangschikken geeft ${cirkelbegin} ${cirkela2}X ${cirkelb2}Y + ${cirkeloverigen} = 0. \`\`\``,
-        },
-        {
-          name: `3. Gegeven lijn ombouwen naar de vorm Y = AX + B:`,
-          value: `Uit ${aformule}X ${bformule} = ${formulenaam} volgt ${formulenaam} = ${aformuleom}X + ${bformuleom}.`,
-        },
-        {
-          name: `4. Substitutie van Y = ${aformuleom} ${bformuleom}${cformuleom} in ${cirkelbegin} ${cirkela2}X ${cirkelb2}Y + ${cirkeloverigen} = 0:`,
-          value: `\`\`\`X² + (${aformuleom}X + ${bformuleom}${cformuleom})² ${cirkela2}X ${cirkelb2}(${aformuleom} ${bformuleom}${cformuleom}) + ${cirkeloverigen} = 0\`\`\``,
-        },
-        {
-          name: `--X-`,
-          value: `\`\`\`X² + ${aformuleomkwa}X² - ${aformuleom2}${formulenaam}X + ${formulenaam}² ${cirkela2}X ${cirkelb2keeraformuleom}X + ${cirkelb2wis}${formulenaam} + ${cirkeloverigen} = 0 \`\`\``,
+          value: `${cirkelnaam}: (X - ${cirkela})² + (Y- ${cirkelb})² = ${cirkelc}`,
         },
         {
           name: `---`,
-          value: `\`\`\`${aformuleomkwatot}X² + (${aformuleom2}${formulenaam} + ${cirkela2cirkelb2keeraformuleom})X + ${formulenaam}² + ${cirkelb2wis}${formulenaam} + ${cirkeloverigen} = 0 \`\`\``,
+          value: `X² - ${cirkela2}X + ${cirkelakwa} + Y² - ${cirkelb2}Y + ${cirkelbkwa} = ${cirkelc}`,
+        },
+        {
+          name: `---`,
+          value: `X² + Y² - ${cirkela2}X - ${cirkelb2}Y + ${cirkeloverigen} = 0`,
+        },
+        {
+          name: `4. Substitutie van Y = ${formulea}X + ${formulex} in X² + Y² - ${cirkela2}X - ${cirkelb2}Y + ${cirkeloverigen} = 0`,
+          value: `X² + (${formulea}X + ${formulex}) - ${cirkela2}X - ${cirkelb2}(${formulea}X + ${formulex}) + ${cirkeloverigen} = 0`,
+        },
+        {
+          name: `---`,
+          value: `X² + ${formuleakwa}X² - ${formulea2}${formulex}X + ${formulex}² - ${cirkela2}X - ${formulea2}X + ${cirkelb2}${formulex} + ${cirkeloverigen} = 0`,
+        },
+        {
+          name: `---`,
+          value: `${xkwavol}X² + (- ${formulea2}X ${formulea2vol})X + ${formulex}² + ${cirkelb2}${formulex} + ${cirkeloverigen} = 0`,
         },
         {
           name: `ABC Formule Toepassen`,
-          value: `\`\`\`(B)² - 4AC\`\`\``,
+          value: `(B)² - 4 ⋅ A ⋅ C`,
         },
         {
           name: `Invullen geeft:`,
-          value: `\`\`\`(${aformuleom2}${formulenaam} + ${cirkela2cirkelb2keeraformuleom})² - 4 ⋅ ${aformuleomkwatot} ⋅ (${formulenaam}² + ${cirkelb2wis}${formulenaam} + ${cirkeloverigen})\`\`\``,
+          value: `(- ${formulea2}X ${formulea2vol})² - 4 ⋅ ${xkwavol} ⋅ (${formulex}² + ${cirkelb2}${formulex} + ${cirkeloverigen})`,
         },
         {
-          name: `Oplossen stap 1 geeft:`,
-          value: `\`\`\`${aformuleom2kwa}${formulenaam}² + ${btwoabcformule}${formulenaam} + ${bthreeabcformule} + ${aabcformule}${formulenaam}² + ${coneabcformule}${formulenaam} + ${ctwoabcformule}\`\`\``,
+          name: `Vereenvoudigen:`,
+          value: `${formulea2kwa}${formulex} + ${formulea2kwac} + ${formulea2volkwa} `,
         },
-        {
-          name: `Oplossen stap 2 geeft:`,
-          value: `\`\`\`${aabcformuleaf}${formulenaam}² + ${babcformuleaf}${formulenaam} + ${cabcformuleaf}\`\`\``,
-        },
-        {
-          name: `D = 0 geeft:`,
-          value: `\`\`\`${aabcformuleaf}${formulenaam}² + ${babcformuleaf}${formulenaam} + ${cabcformuleaf} = 0\`\`\``,
-        },
-        {
-          name: `Je kan hierop de ABC-Formule toepassen, of het boek volgen. Boek doet het met de product-som-methode. Zie hieronder. Vereenvoudigen geeft:`,
-          value: `\`\`\`${aabcformuleafdeelaabc}${formulenaam}² + ${babcformuleafdeelaabc}${formulenaam} + ${cabcformuleafdeelaabc} = 0\`\`\``,
-        },
+        // {
+        //   name: `---`,
+        //   value: ``,
+        // },
+        // {
+        //   name: `---`,
+        //   value: ``,
+        // },
+        // {
+        //   name: `Je kan hierop de ABC-Formule toepassen, of het boek volgen. Boek doet het met de product-som-methode. Zie hieronder. Vereenvoudigen geeft:`,
+        //   value: `\`\`\`${aabcformuleafdeelaabc}${formulenaam}² + ${babcformuleafdeelaabc}${formulenaam} + ${cabcformuleafdeelaabc} = 0\`\`\``,
+        // },
         {
           name: `Geschikt paar vinden (product som-methode):`,
-          value: `In het geval van D = ${formulenaam}² - 38${formulenaam} + 72 --> Q = 2 V Q = 36.`,
+          value: `In het geval van D = ${formulex}² - 38${formulex} + 72`,
+        },
+        {
+          name: `---`,
+          value: `Q = 2 V Q = 36`,
         },
         {
           name: `**--------**`,
