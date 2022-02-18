16c92def47132089d91fb554e08dccfd728f02f3
Puffin Kwadraat <sv.kruik@gmail.com>
2022-02-18 11:00:51 +0100

    Two new flags: everyone and here
    The full update is on its way, just needs testing.

--- Bot Files/The Hangout Server/Ciconia - All/Commands/other-commandtemplate.js
+++ Bot Files/The Hangout Server/Ciconia - All/Commands/other-commandtemplate.js
@@ -9,10 +9,33 @@ module.exports = {
   execute(message, args, cmd, client, Discord) {
     if (args[0] === "delete") {
       message.delete();
-  } else if (args[0] === "del" || args[0] === "d") {
+    } else if (args[0] === "del" || args[0] === "d") {
       message.delete();
-  }
+    } else if (args[0] === "everyone") {
+      message.channel.send(`@everyone, ${config.commandcommandtemplate.commandcommandtemplate}`)
+    } else if (args[0] === "here") {
+      message.channel.send(`@here, ${config.commandcommandtemplate.commandcommandtemplate}`)
+    } else {
+      message.channel.send(config.commandcommandtemplate.commandcommandtemplate);
+    }
 
-    message.channel.send(config.commandcommandtemplate.commandcommandtemplate);
+    const flags = ["here", "everyone", "delete"];
+    if (args[0] === "everyone") {
+      message.channel.send("@everyone")
+    } else if (args[0] === "here") {
+      message.channel.send("@here")
+    } else if (args[0] === "delete" || args[0] === "del") {
+      message.channel.send("delete")
+    }
+
+    if (args[0] === "everyone") {
+      if (args[1] === "here") {
+        return message.channel.send("You cannot tag both everyone and here.")
+      }
+    } else if (args[0] === "here") {
+      if (args[1] === "everyone") {
+        return message.channel.send("You cannot tag both everyone and here.")
+      }
+    }
   },
 };
