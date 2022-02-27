4ab4979e2608dd7931021498bd14228e54f5bdb9
PuffinKwadraat <sv.kruik@gmail.com>
2022-02-27 18:33:15 +0100

    Misc fix to errorcode

--- Bot Files/The Hangout Server/Ciconia - All/Commands/other-errorcode.js
+++ Bot Files/The Hangout Server/Ciconia - All/Commands/other-errorcode.js
@@ -51,64 +51,64 @@ module.exports = {
         },
         {
           name: `> ${err1}`,
-          value: `${info1}`,
+          value: `**1.** ${info1}`,
         },
         {
           name: `> ${err2}`,
-          value: `${info2}`,
+          value: `**2.** ${info2}`,
         },
         {
           name: `> ${err3}`,
-          value: `${info3}`,
+          value: `**3.** ${info3}`,
         },
         {
           name: `> ${err4}`,
-          value: `${info4}`,
+          value: `**4.** ${info4}`,
         },
         {
           name: `> ${err5}`,
-          value: `${info5}`,
+          value: `**5.** ${info5}`,
         },
         {
           name: `> ${err6}`,
-          value: `${info6}`,
+          value: `**6.** ${info6}`,
         },
         {
           name: `> ${err7}`,
-          value: `${info7}`,
+          value: `**7.** ${info7}`,
         },
         {
           name: `> ${err8}`,
-          value: `${info8}`,
+          value: `**8.** ${info8}`,
         },
         {
           name: `> ${err9}`,
-          value: `${info9}`,
+          value: `**9.** ${info9}`,
         },
         {
           name: `> ${err10}`,
-          value: `${info10}`,
+          value: `**10.** ${info10}`,
         },
-        {
-          name: `> ${err11}`,
-          value: `${info11}`,
-        },
-        {
-          name: `> ${err12}`,
-          value: `${info12}`,
-        },
-        {
-          name: `> ${err13}`,
-          value: `${info13}`,
-        },
-        {
-          name: `> ${err14}`,
-          value: `${info14}`,
-        },
-        {
-          name: `> ${err15}`,
-          value: `${info15}`,
-        },
+        // {
+        //   name: `> ${err11}`,
+        //   value: `**11** ${info11}`,
+        // },
+        // {
+        //   name: `> ${err12}`,
+        //   value: `**12.** ${info12}`,
+        // },
+        // {
+        //   name: `> ${err13}`,
+        //   value: `**13.** ${info13}`,
+        // },
+        // {
+        //   name: `> ${err14}`,
+        //   value: `**14.** ${info14}`,
+        // },
+        // {
+        //   name: `> ${err15}`,
+        //   value: `**15.** ${info15}`,
+        // },
         {
           name: `**-------**`,
           value: `We would really appreciate it, if you contact any of the bot developers if you encounter any of these errors. We can then get to work to fix it ASAP.`,
@@ -116,26 +116,41 @@ module.exports = {
       )
       .setFooter(config.embed.embedfooter);
 
+    let flagmessage = newEmbed
+    const flags = ["everyone", "here", "delete"];
+
     if (!args[0]) {
-      message.channel.send(newEmbed);
+      message.channel.send(flagmessage);
     } else if (args[0] === err1) {
-      message.channel.send(`Error Code \`${err1}\`: ${info1}`)
+      let flagmessage = `Error Code \`${err1}\`: ${info1}`
+      message.channel.send(flagmessage)
     } else if (args[0] === err2) {
-      message.channel.send(`Error Code \`${err2}\`: ${info2}`)
+      let flagmessage = `Error Code \`${err2}\`: ${info2}`
+      message.channel.send(flagmessage)
     } else if (args[0] === err3) {
-      message.channel.send(`Error Code \`${err3}\`: ${info3}`)
+      let flagmessage = `Error Code \`${err3}\`: ${info3}`
+      message.channel.send(flagmessage)
     } else if (args[0] === err4) {
-      message.channel.send(`Error Code \`${err4}\`: ${info4}`)
+      let flagmessage = `Error Code \`${err4}\`: ${info4}`
+      message.channel.send(flagmessage)
     } else if (args[0] === err5) {
-      message.channel.send(`Error Code \`${err5}\`: ${info5}`)
+      let flagmessage = `Error Code \`${err5}\`: ${info5}`
+      message.channel.send(flagmessage)
     } else if (args[0] === err6) {
-      message.channel.send(`Error Code \`${err6}\`: ${info6}`)
+      let flagmessage = `Error Code \`${err6}\`: ${info6}`
+      message.channel.send(flagmessage)
     } else if (args[0] === err7) {
-      message.channel.send(`Error Code \`${err7}\`: ${info7}`)
+      let flagmessage = `Error Code \`${err7}\`: ${info7}`
+      message.channel.send(flagmessage)
     } else if (args[0] === err8) {
-      message.channel.send(`Error Code \`${err8}\`: ${info8}`)
+      let flagmessage = `Error Code \`${err8}\`: ${info8}`
+      message.channel.send(flagmessage)
     } else if (args[0] === err9) {
-      message.channel.send(`Error Code \`${err9}\`: ${info9}`)
+      let flagmessage = `Error Code \`${err9}\`: ${info9}`
+      message.channel.send(flagmessage)
+    } else if (args[0] === err10) {
+      let flagmessage = `Error Code \`${err10}\`: ${info10}`
+      message.channel.send(flagmessage)
     } else if (args[0] !== err1 || err2 || err3 || err4 || err5 || err6 || err7 || err8 || err9 || err10 || err11 || err12 || err13 || err14 || err15) {
       message.channel.send(`I haven't heard of the Error Code: \`${args[0]}\`.`)
     }
