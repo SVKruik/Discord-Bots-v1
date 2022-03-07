6aa5c981671bfe28281beebbb1d05e12f9bdfe01
Puffin Kwadraat <sv.kruik@gmail.com>
2022-03-07 12:02:47 +0100

    Didn't work out. Going back to last commit

--- Bot Files/The Hangout Server/Ciconia - All/Events/client/ready.js
+++
@@ -1,9 +0,0 @@
-const config = require("./../../Other/config.js");
-
-module.exports = () =>{
-    const fs = require("fs");
-    const commands = fs.readdirSync("./Commands").length;
-    
-    console.log(`Succesfully loaded ${commands} files.`)
-    console.log(process.env.ONLINE);
-}
