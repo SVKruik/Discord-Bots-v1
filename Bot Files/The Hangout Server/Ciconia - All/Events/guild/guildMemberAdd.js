12283fbb04f8604df59a9a6493cd39c35c184863
PuffinKwadraat <sv.kruik@gmail.com>
2022-03-08 21:17:13 +0100

    Yep

--- Bot Files/The Hangout Server/Ciconia - All/Events/guild/guildMemberAdd.js
+++ Bot Files/The Hangout Server/Ciconia - All/Events/guild/guildMemberAdd.js
@@ -5,16 +5,17 @@ module.exports = async (message, args, cmd, client, Discord, member, profileData
   const tagfull = message.author.tag
   const tag = tagfull.substr(-4);
   let profile = await profileModel.create({
-    userID: message.author.id,
-    serverID: message.guild.id,
-    name: message.author.username,
+    avatar: message.author.avatar,
+    bot: message.author.bot,
     tag: tag,
+    uni: rndInt,
 
-    wallet: 0,
-    bank: 0,
-
-    level: 0,
-    experience: 0,
+    scrapexotic: 0,
+    scraplegendary: 0,
+    scrapepic: 0,
+    scraprare: 0,
+    scrapuncommon: 0,
+    scrapcommon: 0,
 
     music1: "https://www.youtube.com/",
     music2: "https://www.youtube.com/",
@@ -22,6 +23,23 @@ module.exports = async (message, args, cmd, client, Discord, member, profileData
     music4: "https://www.youtube.com/",
     music5: "https://www.youtube.com/",
     music6: "https://www.youtube.com/",
+
+    level: 0,
+    experience: 0,
+
+    wallet: 0,
+    bank: 0,
+
+    boxesultra: 0,
+    boxessuper: 0,
+    boxesnormal: 0,
+
+    shardsdiamond: 0,
+    shardsplatinum: 0,
+    shardsgold: 0,
+    shardssilver: 0,
+    shardsbronze: 0,
+    shardsiron: 0,
   });
   profile.save();
 };
