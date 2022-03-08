016958c99ea01660fe25d7fab6d68e91177b5405
PuffinKwadraat <sv.kruik@gmail.com>
2022-03-08 17:24:42 +0100

    Misc update

--- Bot Files/The Hangout Server/Ciconia - All/Events/client/ready.js
+++ Bot Files/The Hangout Server/Ciconia - All/Events/client/ready.js
@@ -1,9 +1,20 @@
 const config = require("./../../Other/config.js");
 
-module.exports = () =>{
+module.exports = () => {
     const fs = require("fs");
     const commands = fs.readdirSync("./Commands").length;
-    
+
+    var today = new Date();
+    var ss = String(today.getSeconds()).padStart(2, '0');
+    var m = String(today.getMinutes()).padStart(2, '0');
+    var hh = String(today.getHours()).padStart(2, '0');
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = ss + " Seconds / " + m + " Minutes / " + hh + " Hours / " + dd + " Days / " + mm + " Months / " + yyyy;
+
     console.log(`Succesfully loaded ${commands} files.`)
+    console.log(`I started this session on ${today}.`)
     console.log(process.env.ONLINE);
 }
