68c23495eb1b60584a1a03906bc835aa24dcbbcf
Puffin Kwadraat <sv.kruik@gmail.com>
2022-03-09 10:37:18 +0100

    Fix

--- Bot Files/The Hangout Server/Ciconia - All/Commands/dev-testsuggest.js
+++ Bot Files/The Hangout Server/Ciconia - All/Commands/dev-testsuggest.js
@@ -28,7 +28,7 @@ module.exports = {
             channel.send(newEmbed).then((msg) => {
                 msg.react('👍');
                 msg.react('👎');
-                message.channel.send(`Test Suggestion sent in <#${config.base.basetestsuggestchannelid}>.`)
+                message.channel.send(`Test Suggestion sent in <#${config.base.basebottestingthreechannelid}>.`)
             });
         } catch (err) {
             console.log(err)
