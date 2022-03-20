c792fabc752ab440c656d8cdf5080de23311208c
PuffinKwadraat <sv.kruik@gmail.com>
2022-03-20 14:11:41 +0100

    New Bot: Perdix
    Statistics

---
+++ Bot Files/The Hangout Server/Perdix - Statistics/Counters/counters.js
@@ -0,0 +1,24 @@
+// Een counter die de hoeveelheid leden in een Discord server laat zien.
+// Updatesmet een interval naar keuze. 
+
+const config = require("../Other/config.js");
+
+module.exports = async (client) => {
+    const guild = client.guilds.cache.get(config.base.baseguildid);
+    const memberCount = guild.memberCount;
+    const channel1 = guild.channels.cache.get(config.base.basemembercounterchannelid);
+    channel1.setName(`Total Members: ${memberCount.toLocaleString()}`);
+    console.log(`Updated current Member Count in ${guild.name}.`);
+
+    const peopleCount = memberCount - 13;
+    const channel2 = guild.channels.cache.get(config.base.basepeoplecounterchannelid);
+    channel2.setName(`Total People: ${peopleCount.toLocaleString()}`);
+    console.log(`Updated current People Count in ${guild.name}.`);
+
+    setInterval(() => {
+        channel1.setName(`Total Members: ${memberCount.toLocaleString()}`);
+        console.log(`Updating current Member Count in ${guild.name}.`);
+        channel2.setName(`Total People: ${peopleCount.toLocaleString()}`);
+        console.log(`Updating current People Count in ${guild.name}.`);
+    }, config.base.basecounterinterval);
+}
