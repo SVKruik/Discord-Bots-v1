cc9451e647329e87650f9b80ced833a82f04bea5
PuffinKwadraat <sv.kruik@gmail.com>
2022-03-28 20:48:28 +0200

    Added empty commands: shards

---
+++ Bot Files/The Hangout Server/Ciconia - All/Commands/e-sds-shards.js
@@ -0,0 +1,12 @@
+const config = require("../Other/config.js"); // Vaste variabelen opgeslagen
+const profileModel = require("../models/profileSchema");
+
+module.exports = {
+    name: "shards",
+    aliases: config.aliases.aliasesshards,
+    cooldown: config.cooldown.cooldownshards,
+    permissions: config.permissions.permissionshards,
+    description: "Displays your current shards.",
+    execute(message, args, cmd, client, Discord, profileData) {
+    },
+};
