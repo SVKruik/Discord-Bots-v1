317f726d6534cd48dd8f8f7548214046702780d5
PuffinKwadraat <sv.kruik@gmail.com>
2022-03-28 20:32:04 +0200

    Added empty files: level

---
+++ Bot Files/The Hangout Server/Ciconia - All/Commands/e-lvl-minusfulllevel.js
@@ -0,0 +1,12 @@
+const config = require("../Other/config.js");
+const profileModel = require("../models/profileSchema");
+
+module.exports = {
+    name: "minuslevelandexperience",
+    aliases: config.aliases.aliasesminusfulllevel,
+    cooldown: config.cooldown.cooldownminusfulllevel,
+    permissions: config.permissions.permissionminusfulllevel,
+    description: "Decrease the experience and level.",
+    async execute(message, args, cmd, client, Discord, profileData) {
+    },
+};
