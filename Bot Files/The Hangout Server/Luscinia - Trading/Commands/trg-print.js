e67f2dde9dada95ba13fee5debd1293edf913711
PuffinKwadraat <sv.kruik@gmail.com>
2022-06-09 08:18:42 +0200

    Market stock added
    Market cap

--- Bot Files/The Hangout Server/Luscinia - Trading/Commands/trg-print.js
+++ Bot Files/The Hangout Server/Luscinia - Trading/Commands/trg-print.js
@@ -14,7 +14,8 @@ module.exports = {
     async execute(message, args, cmd, client, Discord, profileData) {
         try {
             const attachment1 = new MessageAttachment('marketprice.json')
-            const attachment2 = new MessageAttachment('Other/materiallist.txt')
+            const attachment2 = new MessageAttachment('marketstock.json')
+            message.channel.send(`The first file is the current marketprice, which constanly changes depending on the stock. The second file is the marketstock. That is the amount of material there is on the market. If something is empty, it will get refilled soon.`)
             message.channel.send({ files: [attachment1, attachment2] });
         } catch (err) {
             console.log(err)
