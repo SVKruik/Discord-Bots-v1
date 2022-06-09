44b3f9b599d620219143a77183dfbdd1bee0c823
PuffinKwadraat <sv.kruik@gmail.com>
2022-06-09 08:12:26 +0200

    Updated print cmd
    It now sends the file

--- Bot Files/The Hangout Server/Luscinia - Trading/Commands/trg-print.js
+++ Bot Files/The Hangout Server/Luscinia - Trading/Commands/trg-print.js
@@ -1,19 +1,21 @@
-// Print de hele marketprice JSON in de log.
+// Print de hele marketprice JSON in de chat.
 
 const config = require("../Other/config.js");
 const profileModel = require("../models/profileSchema");
 const editJsonFile = require("edit-json-file");
+const { MessageAttachment } = require('discord.js')
 
 module.exports = {
     name: "print",
     aliases: config.aliases.aliasesprint,
     cooldown: config.cooldown.cooldownprint,
     permissions: config.permissions.permissionprint,
-    description: "Print the JSON file containing the marketprices in the console.",
+    description: "Print the JSON file containing the marketprices and list in the chat.",
     async execute(message, args, cmd, client, Discord, profileData) {
         try {
-            let file = editJsonFile(`./marketprice.json`);
-            console.log(file.get());
+            const attachment1 = new MessageAttachment('marketprice.json')
+            const attachment2 = new MessageAttachment('Other/materiallist.txt')
+            message.channel.send({ files: [attachment1, attachment2] });
         } catch (err) {
             console.log(err)
             message.channel.send(`Error executing command. EC: \`${config.errorcodes.err2}\`.`) // Error Systeem
