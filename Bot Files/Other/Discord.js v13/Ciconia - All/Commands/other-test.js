a95152faaf50c2ef7a17079eb53128e08bd72a64
PuffinKwadraat <sv.kruik@gmail.com>
2022-06-12 16:31:49 +0200

    Misc

--- Bot Files/Other/Discord.js v13/Ciconia - All/Commands/other-test.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "test",
-    execute(message, args, cmd, client, Discord) {
-        message.channel.send({ content: "Hello" })
-        console.log("Yay");
-    },
-};
