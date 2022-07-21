f0159d48e315296a238fd259dd3fbcfe3fd78d8f
PuffinKwadraat <sv.kruik@gmail.com>
2022-07-21 15:45:36 +0200

    Part 3

--- Bot Files/The Hangout Server/Hirundo - Testing/Commands/adm-awake.js
+++ Bot Files/The Hangout Server/Hirundo - Testing/Commands/adm-awake.js
@@ -1,7 +1,4 @@
-// Check wie er allemaal wakker is. Als ik meerdere bots aan heb, kan ik kijken wie er allemaal reageren.
-// Voor admin omdat je hier mee kan spammen.
-// Als een bot niet reageert, betekent dat er een fatale fout in de code zit, waardoor hij helemaal niet kan reageren.
-// Vroeger had ik de command `ping` voor dit doeleinde, maar die laat nu echt de ping van de bot en API zien.
+// Bot check.
 
 const config = require("../Other/config.js"); // Vaste variabelen opgeslagen
 
---
+++ Bot Files/The Hangout Server/Larum - Events/commands/e-trg-sell.js
@@ -0,0 +1,13 @@
+const config = require("../Other/config.js");
+const profileModel = require("../models/profileSchema");
+const editJsonFile = require("edit-json-file");
+
+module.exports = {
+    name: "tradingsell",
+    aliases: config.aliases.aliasestradingsell,
+    cooldown: config.cooldown.cooldowntradingsell,
+    permissions: config.permissions.permissiontradingsell,
+    description: "Sell something to the market.",
+    async execute(message, args, cmd, client, Discord, profileData) {
+    },
+};
